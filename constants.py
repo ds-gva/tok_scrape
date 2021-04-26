bdf075403467b7ea8faa12947af428da8e4652a3
daniel-lyfto <daniel.swann@lyftonomie.com>
2021-04-26 09:23:48 +0200

    use of web3 & json-rpc instead of chromedriver

---
+++ constants.py
@@ -0,0 +1,6 @@
+BLOCKCHAIN_URL = 'https://bsc-dataseed1.defibit.io:443/'
+PCS_FACTORY_ADDRESS = '0xbcfccbde45ce874adcb698cc183debcf17952812'
+TOPICS = ["0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9"]
+WBNB_ADDRESS_LONG = '0x000000000000000000000000bb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+DUMMY_PCS_ABI = '[{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]'
+SECONDS_PER_BLOCK = 3
